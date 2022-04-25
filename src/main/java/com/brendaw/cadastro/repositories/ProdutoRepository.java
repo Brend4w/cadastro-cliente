551bbfdbcbd77718e0cfaeb2996ae571129a9a84
Brendaw <brendaw.oliveira.5@gmail.com>
2022-04-25 19:58:35 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/brendaw/cadastro/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.brendaw.cadastro.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brendaw.cadastro.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
