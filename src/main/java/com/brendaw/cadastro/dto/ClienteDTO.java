35e723e9f61971172c4d2c0f5a64279e8a542102
Brendaw <brendaw.oliveira.5@gmail.com>
2022-05-25 12:48:02 -0300

    PUT, DELETE e GET para Cliente

---
+++ src/main/java/com/brendaw/cadastro/dto/ClienteDTO.java
@@ -0,0 +1,66 @@
+package com.brendaw.cadastro.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.brendaw.cadastro.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 80 caracteres")
+	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="E-mail inválido")
+	private String email;
+	
+
+	public ClienteDTO() {
+	
+}
+	
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/com/brendaw/cadastro/resources/ClienteResource.java
+++ src/main/java/com/brendaw/cadastro/resources/ClienteResource.java
@@ -1,16 +1,22 @@
 package com.brendaw.cadastro.resources;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brendaw.cadastro.domain.Cliente;
+import com.brendaw.cadastro.dto.ClienteDTO;
 import com.brendaw.cadastro.services.ClienteService;
 
 @RestController
@@ -25,4 +31,44 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> uptade(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.uptade(obj);
+		return ResponseEntity.noContent().build();		
+
+}
+
+	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findALL() {
+		List<Cliente> list = service.findALL();
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value="/page",method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+		@RequestParam(value="page", defaultValue = "0")	Integer page,
+		@RequestParam(value="linesPerPage", defaultValue = "24")	Integer linesPerPage,
+		@RequestParam(value="ordemBy", defaultValue = "nome")	String orderBy,
+		@RequestParam(value="direction", defaultValue = "ASC")	String direction) {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	
+	
+	
+	
+	
 }
